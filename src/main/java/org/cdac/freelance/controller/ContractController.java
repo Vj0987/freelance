df514611bed1fcd705be6f9eb0dde198430743dd
Vinit Joshi <vinitjoshi1709@gmail.com>
2026-08-04 10:41:40 +0530

    Updated freelance project

--- src/main/java/org/cdac/freelance/controller/ContractController.java
+++ src/main/java/org/cdac/freelance/controller/ContractController.java
@@ -2,9 +2,11 @@ package org.cdac.freelance.controller;
 
 import org.cdac.freelance.dto.contract.ContractResponseDTO;
 import org.cdac.freelance.dto.contract.CreateContractRequestDTO;
+import org.cdac.freelance.security.CustomUserPrincipal;
 import org.cdac.freelance.services.contract.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,11 +23,12 @@ public class ContractController {
     }
 
     @GetMapping("/provider")
-    public ResponseEntity<List<ContractResponseDTO>> getContractByProviderId(@PathVariable int providerId){
-        return  ResponseEntity.ok(contractService.getAllContractByProviderId(providerId));
+    public ResponseEntity<List<ContractResponseDTO>> getContractByProviderId(@AuthenticationPrincipal CustomUserPrincipal user){
+        return  ResponseEntity.ok(contractService.getAllContractByProviderId(user.getUserId()));
     }
 
-    public ResponseEntity<List<ContractResponseDTO>> getContractByClientId(@PathVariable int clientId){
-        return ResponseEntity.ok(contractService.getAllContractByClientId(clientId));
+    @GetMapping("/client")
+    public ResponseEntity<List<ContractResponseDTO>> getContractByClientId(@AuthenticationPrincipal CustomUserPrincipal user){
+        return ResponseEntity.ok(contractService.getAllContractByClientId(user.getUserId()));
     }
 }
